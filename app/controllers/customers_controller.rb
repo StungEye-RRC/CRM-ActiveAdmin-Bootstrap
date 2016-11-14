b78422e3e312a302b17d07bd3388f98295ef51a4
Kyle Geske <stungeye@gmail.com>
2016-11-14 10:25:53 -0600

    Implemented a session-based visit counter.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,20 @@
 class CustomersController < ApplicationController
+  before_action :initialize_session
+  before_action :increment_visit_count, only: [:index]
+
   def index
     @customers = Customer.all
   end
   # Automatically load the app/views/customers/index.html.erb
+
+  private
+
+  def initialize_session
+    session[:visit_count] ||= 0
+  end
+
+  def increment_visit_count
+    session[:visit_count] += 1
+    @visit_count = session[:visit_count]
+  end
 end

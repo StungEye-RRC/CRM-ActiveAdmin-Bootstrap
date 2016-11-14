c0d302cb6d1529ae8284191c49c7fa2a25e3e8ee
Kyle Geske <stungeye@gmail.com>
2016-11-14 10:50:48 -0600

    Started to implement the 'remember to call' customer stuff.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,20 +1,36 @@
 class CustomersController < ApplicationController
   before_action :initialize_session
   before_action :increment_visit_count, only: [:index]
+  before_action :load_customers_to_call, only: [:index]
 
   def index
     @customers = Customer.all
   end
   # Automatically load the app/views/customers/index.html.erb
 
+  def remember_to_call
+    id = params[:id].to_i
+    session[:customers_to_call] << id  unless session[:customers_to_call].include?(id)
+
+    redirect_to root_url
+  end
+
+  def mark_as_called
+  end
+
   private
 
   def initialize_session
     session[:visit_count] ||= 0
+    session[:customers_to_call] ||= []
   end
 
   def increment_visit_count
     session[:visit_count] += 1
     @visit_count = session[:visit_count]
+  end
+
+  def load_customers_to_call
+    @customers_to_call = Customer.find(session[:customers_to_call])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root to: 'customers#index'
 
+  resources :customers, only: [:index] do
+    member do
+      post :remember_to_call
+      post :mark_as_called
+    end
+  end
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
